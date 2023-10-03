38a83c83baa687b135d89d3be6a1ede4dd0c2211
Gabriel <gabrielcavalheiro20@gmail.com>
2023-10-03 17:47:01 -0300

    mano nem sei se deu certo

---
+++ scr/main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from settings import HOST, PORT, DEBUG
+from flask import Flask, render_template
+
+# import blueprint criado
+from mod_funcionario.funcionario import bp_funcionario
+from mod_cliente.cliente import bp_cliente
+from mod_index.index import bp_index
+from mod_produto.produto import bp_produto
+
+app = Flask(__name__)
+
+# registro das rotas do blueprint
+app.register_blueprint(bp_funcionario)
+app.register_blueprint(bp_cliente)
+app.register_blueprint(bp_index)
+app.register_blueprint(bp_produto)
+
+
+''' chamadas dos formulários '''
+@app.route('/')
+def formIndex():
+    return render_template('formIndex.html'), 200
+
+@app.route('/funcionario/')
+def formListaFuncionario():
+    return render_template ('formListaFuncionario.html'), 200
+
+@app.route('/cliente/')
+def formListaCliente():
+    return render_template('formListaCliente.html'), 200
+
+@app.route('/produto/')
+def formListaProduto():
+    return render_template('formListaProduto.html'), 200
+
+
+
+if __name__ == "__main__":
+    """ Inicia o aplicativo WEB Flask """
+    app.run(host='0.0.0.0', port=8001, debug=True) #tive  qcolocar 8001 pq a 5000 não tava indo :))))
